644eb2afef332c3cb6fe9bb345c79d666ad3dafd
ARSENL <arsenl@jabba.(none)>
2014-07-21 16:42:06 -0700

    Changes to path in wsgi to work with apache server, and added Jenkins icon to base webpage

--- test_results/urls.py
+++ test_results/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^tests/', include('resultsdisplay.urls', namespace="resultsdisplay")),
+    url(r'^', include('resultsdisplay.urls', namespace="resultsdisplay")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- test_results/wsgi.py
+++ test_results/wsgi.py
@@ -7,8 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_results.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
